c8747402d1b1b5cd1a33024a473501e61701192f
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-04-24 18:09:51 -0500

    genera constula sql

--- app.rb
+++ app.rb
@@ -1,13 +1,28 @@
 require 'openai_chatgpt'
 require 'dotenv/load'
 
+db_schema = ''
+File.open('schema.sql', 'r') do |file|
+  # Lee todas las líneas del archivo y las asigna a una variable
+  db_schema = file.read
+end
+db_schema = db_schema + '\n en base al esquema anterior, cual sería la consulta sql para la siguiente pregunta: \n'
+
 client = OpenaiChatgpt::Client.new(api_key: ENV['OPENAI_API_KEY'])
-message = 'y la de Venezuela?'
+puts 'Ingrese su consulta: '
+query = gets.chomp
+input_message = "#{db_schema}\n#{query}"
 
 begin
-  response = client.completions(messages: [{role: "user", content: message}])
+  response = client.completions(messages: [{role: "user", content: input_message}])
   response.results.each do |result|
-    puts result.content
+    sql = result.content.match(/SELECT.*?;/)
+    # puts result.content
+    if sql
+      puts sql[0] # Imprime el SQL encontrado
+    else
+      puts "No se encontró ninguna consulta SQL en la cadena."
+    end
   end
 rescue OpenaiChatgpt::Error => e
   puts e.message
