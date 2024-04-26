1b893a8d91ef26bc45aaa0a07e07233a623fa49d
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-04-25 22:07:59 -0500

    consulta muestra resultados en pantalla

--- src/pages/app/Conversation.jsx
+++ src/pages/app/Conversation.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { InputGroup, FormControl, Row, Col, Form, Button } from 'react-bootstrap';
+import { InputGroup, FormControl, Row, Col, Table, Button } from 'react-bootstrap';
 import { sendQuestion } from '../../services/ChatService';
 
 class Conversation extends Component {
@@ -10,6 +10,9 @@ class Conversation extends Component {
       question: '',
       message: '',
       messageClass: '',
+      messageClass: '',
+      columns: [],
+      resultSet: [],
       disabled: false,
     };
     this.questionInputRef = React.createRef();
@@ -20,10 +23,14 @@ class Conversation extends Component {
   }
 
   sendMessageClick = () => {
-    const { question } = this.state;
+    const { question, } = this.state;
     sendQuestion(question)
-      .then(data => {
-        console.log(data);
+      .then(responseData => {
+        console.log(responseData);
+        this.setState({
+          columns: responseData.data.columns,
+          resultSet: responseData.data.result_set,
+        });
       })
       .catch(error => {
         console.error(error);
@@ -46,9 +53,40 @@ class Conversation extends Component {
   }
 
   render() {
+    const { 
+      columns, 
+      resultSet, 
+    } = this.state;
+
+    
+
     return (
       <>
         <h2>Nueva Conversación</h2>
+        {resultSet.length === 0 ? (
+          <></>
+        ) : (
+          <Row>
+            <Table striped hover>
+              <thead>
+                <tr>
+                {columns.map((columnName, index) => (
+                  <th key={index}>{columnName}</th>
+                ))}
+                </tr>
+              </thead>
+              <tbody>
+                {resultSet.map((record, resultSetIndex) => 
+                  <tr>
+                  {columns.map((columnName, columnIndex) => (
+                    <td key={columnIndex}>{record[columnName]}</td>
+                  ))}
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+          </Row>
+        )}
         <Row>
           <Col>
             <InputGroup className="mb-3">
